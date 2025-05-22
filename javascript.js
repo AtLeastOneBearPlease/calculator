5434dbf774bf2b1cf224fd39141622c938e894c2
AtLeastOneBearPlease <197534792+AtLeastOneBearPlease@users.noreply.github.com>
2025-05-22 12:42:08 -0500

    feat: Add code for operators and calculations

---
+++ javascript.js
@@ -0,0 +1,40 @@
+function add(x, y){
+    return x + y;
+}
+
+function subtract(x, y){
+    return x - y;
+}
+
+function multiply(x, y){
+    return x * y;
+}
+
+function divide(x, y){
+    return x / y;   
+}
+
+function operate(operator, num1, num2){
+    switch(operator) {
+        case "+":
+            return add(num1, num2);
+            break;
+        case "-":
+            return subtract(num1, num2);
+            break;
+        case "*":
+            return multiply(num1, num2);
+            break;
+        case "/":
+            return divide(num1, num2);
+            break;
+    }
+}
+
+
+/*
+
+For the function of the calculator, you know that we will have to pull information from the buttons
+and display it in the calculator text box; 
+
+*/
