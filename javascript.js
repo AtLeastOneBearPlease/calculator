19da9b84c96919ea08c717fd534e5e26c57fd586
AtLeastOneBearPlease <197534792+AtLeastOneBearPlease@users.noreply.github.com>
2025-05-22 16:03:41 -0500

    feat: Add plus button logic

--- javascript.js
+++ javascript.js
@@ -1,7 +1,9 @@
 let calcDisplay = document.querySelector("input");
 
-let num1 = 0;
-let num2 = 0;
+let firstNumber = "";
+let currentNumString = "";
+let currentOperator = "";
+let enteringFirstNumber = true;
 
 function add(x, y){
     return x + y;
@@ -19,7 +21,7 @@ function divide(x, y){
     return x / y;   
 }
 
-function operate(operator, num1, num2){
+function operate( num1, operator, num2){
     switch(operator) {
         case "+":
             return add(num1, num2);
@@ -31,6 +33,7 @@ function operate(operator, num1, num2){
             return multiply(num1, num2);
             break;
         case "/":
+            if(num2 === 0) return "ERROR";
             return divide(num1, num2);
             break;
     }
@@ -40,11 +43,37 @@ function numButtonEntered(event){
     let button = event.target;
 
     if(button.className === "number-button"){
-        let string = calcDisplay.value;
+        currentNumString += button.innerText;
+        setCalcDisplay();
+    }
+}
 
-        string += button.innerText;
-        calcDisplay.value = string;
-        calcDisplay.readOnly = true;
+function setCalcDisplay(){
+    calcDisplay.value = `${currentNumString} ${currentOperator} ${firstNumber}`
+}
+
+function operatorEntered(event){
+    let button = event.target;
+
+    if(button.className === 'operator-button'){
+        if(button.innerText === '+'){
+            if(enteringFirstNumber){
+                currentOperator = "+";
+                firstNumber = currentNumString;
+                currentNumString = "";
+                setCalcDisplay();
+                enteringFirstNumber = false;
+            } else {
+                let num1, num2 = 0;
+                num1 = parseFloat(firstNumber);
+                num2 = parseFloat(currentNumString);
+                currentNumString = operate(num1, currentOperator, num2).toString();
+                firstNumber = "";
+                currentOperator = "";
+                setCalcDisplay();
+                enteringFirstNumber = true;
+            }
+        }
     }
 }
 
@@ -52,9 +81,15 @@ let numberInputSection = document.querySelector("#calculator-input");
 let operatorInputSection = document.querySelector("#operator-buttons");
 
 numberInputSection.addEventListener('click', numButtonEntered);
+operatorInputSection.addEventListener('click', operatorEntered);
 
 /*
 
-
+We start with nothing. We need the behavior to be, press as many numbers as you like. Once you hit the operator,
+save that number and then start taking in the second string of numbers. 
+Once the person enters either the equals sign, or another operator, we need to invoke the operation and start the new set of numbers. 
+If the equals sign or operator is pushed, then we need to set the first number in the textbox to the total and 
+expect a new operator. 
+If we get a new number at that point, we need to clear out the values and start over. The same for if they hit the clear button.
 
 */
