afb6503239f6f908884840e42d1c629bf5703277
AtLeastOneBearPlease <197534792+AtLeastOneBearPlease@users.noreply.github.com>
2025-05-23 07:49:17 -0500

    feat: Add comments and TODO

--- javascript.js
+++ javascript.js
@@ -60,6 +60,7 @@ function numButtonEntered(event){
     let button = event.target;
 
     if(button.className === "number-button"){
+        //Allow you to overwrite the displayed result after a calculation
         if(isFirstCalculation === false && currentOperator === "")
         {
             firstNumber = button.innerText;
@@ -67,6 +68,7 @@ function numButtonEntered(event){
             isFirstValue = true;
         } 
         else {
+            //Make sure that we are updating the correct string
             if(isFirstValue){
                 firstNumber += button.innerText;
             } else {
@@ -82,6 +84,7 @@ function setCalcDisplay(){
     calcDisplay.value = `${secondNumber} ${currentOperator} ${firstNumber}`
 }
 
+//Handle the operator behaviors
 function operatorEntered(event){
     let button = event.target;
 
@@ -95,6 +98,7 @@ function operatorEntered(event){
             }
 
             //set the operator to ours as long as the first value has been entered and the second number is empty
+            //This can happen if the operator is pressed when there has been no value input
             if(!isFirstValue && secondNumber === ''){
                 currentOperator = '+'
             }
@@ -159,11 +163,12 @@ clearButton.addEventListener('click', clear);
 
 /*
 
-We start with nothing. We need the behavior to be, press as many numbers as you like. Once you hit the operator,
-save that number and then start taking in the second string of numbers. 
-Once the person enters either the equals sign, or another operator, we need to invoke the operation and start the new set of numbers. 
-If the equals sign or operator is pushed, then we need to set the first number in the textbox to the total and 
-expect a new operator. 
-If we get a new number at that point, we need to clear out the values and start over. The same for if they hit the clear button.
+TODO:
+
+Add hover effects to buttons.
+Add a delete button
+Add a decimnal button
+Change the background of the webpage to nice gradient
+Change the button cursor to a pointer
 
 */
